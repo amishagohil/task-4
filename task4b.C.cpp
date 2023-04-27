7db864319783ad8008da7dbe4f170979f90ba931
amishagohil <131762811+amishagohil@users.noreply.github.com>
2023-04-27 17:15:27 +0530

    task-4(24,april)

---
+++ task4b.C.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	float pi,r,area;
+	
+	printf("the number A =");
+	scanf("%f",&pi);
+	printf("the number B =");
+	scanf("%f",&r);
+	area=2*pi*r;
+	printf("area of pc=%.2f",area);
+}
---
+++ task4d.C.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+ 	float p,r,n,area;
+ 	
+ 	printf("the number A=");
+ 	scanf("%f",&p);
+ 	printf("the number B=");
+ 	scanf("%f",&r);
+ 	printf("the number C=");
+ 	scanf("%f",&n);
+ 	
+ 	area=p*r*n/100;
+ 	printf("area of si=%f",area);
+ 	
+ }
